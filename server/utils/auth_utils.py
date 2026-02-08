4e2e67a72133518c58fab7cf24fca0d5eec623f6
rahulgope45 <rahuleile456@gmail.com>
2026-02-09 00:47:21 +0530

    User auth middleware added

---
+++ server/utils/auth_utils.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from jose import JWTError,jwt 
+from passlib.context import CryptContext
+from typing import Optional
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password:str) -> str:
+    return pwd_context.hash(password)
+
+def verify(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password,hash_password)
+
+# ---- I have added utcoffset insted of utcnow cuz utc is comming deprecated
+
+def create_access_token(data: dict,expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcoffset() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
