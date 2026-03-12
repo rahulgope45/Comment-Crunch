80a599304124fc76ac9f0a47d863a9d4b663b391
rahulgope45 <rahuleile456@gmail.com>
2026-03-12 15:59:52 +0530

    Added the useAuth state for auth is completed

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,5 +1,22 @@
 import { useAuthStore } from "../store/authStore";
 
+
 export const useAuth =() =>{
-    
-}
+    const user = useAuthStore((state) => state.user);
+    const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
+    const isLoading = useAuthStore((state) => state.isLoading)
+    const error = useAuthStore((state) => state.error)
+    const login = useAuthStore((state) => state.login)
+    const logout = useAuthStore((state) => state.logout)
+    const checkAuth = useAuthStore((state) => state.checkAuth)
+
+    return{
+        user,
+        isAuthenticated,
+        isLoading,
+        error,
+        login,
+        logout,
+        checkAuth,
+    };
+};
