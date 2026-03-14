84db53c9925754d39eed0368916aabdb166251f6
rahulgope45 <rahuleile456@gmail.com>
2026-03-14 16:03:21 +0530

    Services section added

---
+++ frontend/src/components/Services.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+
+type DataIteam = {
+        icon: string;
+        title: string;
+        content: string
+    }
+
+    const data:DataIteam[] = [
+        {
+            icon: "/icon3.png",
+            title: "Comparative Analysis",
+            content: "Compare sentiment across multiple videos",
+        },
+        {
+            icon: "/icon2.png",
+            title: "Engagement Metrics",
+            content: "Track comment volume, frequency, and sentiment shifts over time.",
+        },
+        {
+            icon: "/icon1.png",
+            title: "Keyword & Topic Insights",
+            content: "Identify trending words, recurring themes, and emotional triggers in the comment section.",
+        }
+
+    ]
+
+const Services: React.FC=()=> {
+
+    
+    return (
+        <div className='flex flex-col items-center justify-center gap-20'>
+            <h1 className='font-garamond font-bold text-[40px] '>Services</h1>
+            <div className='flex gap-20'>
+            {data.map((iteam,index)=>(
+                <div
+                key={index}
+                className='flex flex-col items-center font-garamond w-75 h-87.5 bg-stone-500 rounded-[30px] p-5 shadow-2xl shadow-blue-950'
+                >
+                 <div>
+                 <img
+                  className='w-32.5 h-32.5 rounded-full bg-gray-100 flex items-center justify-center mx-auto mb-4'
+                 src={iteam.icon}/>
+                 </div>
+
+                 <div>
+                    <h1 className='text-[25px] font-medium text-center'>
+                        {iteam.title}
+                    </h1>
+                 </div>
+                 <div>
+                    <p className='text-[20px] font-normal text-center'>
+                        {iteam.content}
+                    </p>
+                 </div>
+                </div>
+            ))}
+        </div>
+        </div>
+    )
+}
+
+export default Services
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,47 +1,13 @@
 
 import { NavLink } from 'react-router-dom'
+import HeroSection from '../components/HeroSection'
+import Services from '../components/Services'
 
 function Home() {
   return (
-    <div className='flex mt-30 items-center justify-center'>
-      <div className='flex flex-col items-center font-garamond gap-4'>
-        <p className='font-medium text-[25px]'>
-          For Creators & Agencies
-        </p>
-        <p className='text-center font-bold text-[40px] text-indigo-800'>
-          Know Your Community<br />
-          Better With<br />
-          Comment Crunch
-        </p>
-        <p className='text-center font-normal text-[20px]'>
-          Made for agencies and creatives, designed to showcase<br />
-          your work with a polished, professional look.
-        </p>
-        <NavLink to={"/app"}
-          className="w-[200px] h-[50px] bg-indigo-800 hover:bg-indigo-900 flex items-center justify-center text-white rounded-3xl gap-2"
-
-        >
-          Start Now
-          <div className="w-5 h-5 rounded-full bg-white flex items-center justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-3 h-3 rotate-[-50deg]"
-              viewBox="0 0 22 22"
-              fill="none"
-              stroke="black"
-              strokeWidth="2"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14M12 5l7 7-7 7" />
-            </svg>
-          </div>
-        </NavLink>
-
-        <img
-        src='/saphire.gif'
-        alt='Gif-Herosection'
-        className='w-[600px] h-[240px] rounded-[60px]'
-        />
-      </div>
+    <div className='flex flex-col mt-30 items-center justify-center gap-40'>
+      <HeroSection/>
+      <Services/>
     </div>
   )
 }
