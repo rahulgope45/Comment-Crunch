b3724fb866ef83544af121d1a5b516aaefe48d6e
rahulgope45 <rahuleile456@gmail.com>
2026-03-18 02:43:23 +0530

    req.txt updated

--- server/main.py
+++ server/main.py
@@ -31,6 +31,7 @@ origins=[
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
+    "https://comment-crunch-six.vercel.app",
     os.getenv("FRONTEND_URL")
 ]
 
