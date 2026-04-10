adee9d0571ef5f885343a05d2ffbb64ce2208df0
rahulgope45 <rahuleile456@gmail.com>
2026-04-11 01:15:24 +0530

    Added prisma connected database

--- tsApis/dist/index.js
+++ tsApis/dist/index.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express, {} from 'express';
+import { prisma } from './lib/prisma.js';
 const PORT = 3000;
 const app = express();
 app.use(express.json());
