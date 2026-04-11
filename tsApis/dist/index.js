a36d7935b2b98c1c6002f4179315aa4051f6eddc
rahulgope45 <rahuleile456@gmail.com>
2026-04-12 00:37:10 +0530

    Added a cool CLI type log

--- tsApis/dist/index.js
+++ tsApis/dist/index.js
@@ -2,13 +2,30 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express, {} from 'express';
 import { prisma } from './lib/prisma.js';
+import figlet from 'figlet';
+import chalk from 'chalk';
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    figlet("Comment Crunch", (err, data) => {
+        if (err) {
+            res.send("Error generating banner");
+            return;
+        }
+        // Send colored ASCII art banner
+        res.send(`<pre style="color: red;">${data}</pre>`);
+    });
 });
 app.listen(PORT, () => {
-    console.log(`Server is running at ${PORT}`);
+    figlet("Comment Crunch", { font: "Standard" }, (err, data) => {
+        if (err) {
+            console.log(chalk.red("Error generating banner"));
+            return;
+        }
+        // Style ASCII art with chalk
+        console.log(chalk.red.bold(data));
+    });
+    console.log(chalk.blue.bold(`Server is running at http://localhost:${PORT}`));
 });
 //# sourceMappingURL=index.js.map
